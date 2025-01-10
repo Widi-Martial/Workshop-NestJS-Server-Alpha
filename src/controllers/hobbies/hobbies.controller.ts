deffec7ff0f76e554d825aa7a130ebe256689c9e
Widi MARTIAL <widi.martial@gmail.com>
2025-01-10 18:42:22 +0100

    Update hobbies.controller.ts

--- src/controllers/hobbies/hobbies.controller.ts
+++ src/controllers/hobbies/hobbies.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('hobbies')
-export class HobbiesController {}
+export class HobbiesController {
+  @Get()
+  findAll(): string {
+    return 'this is all hobbies';
+  }
+}
